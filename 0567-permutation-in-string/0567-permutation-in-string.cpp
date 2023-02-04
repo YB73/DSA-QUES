bada09309deaf52294392e00708eab00e9f6ac65
Yuvanna Bawa <76108856+YB73@users.noreply.github.com>
2023-02-04 12:44:35 +0530

    Time: 2464 ms (5.02%), Space: 245.8 MB (5.01%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
+    bool isPermutation(string s, string t){
+        unordered_map<char, int> mp;
+        for(int i=0; i<s.size(); i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }       
+        for(auto it: mp){
+            if(it.second != 0) return false;
+        }       
+        return true;
+        
+    }
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char, int> m;
-        for(int i=0; i<s1.length(); i++){
-            m[s1[i]]++;
-        }
-        int i=0, j=0;
-        int k=s1.length();
-        int cnt = m.size();
-         while(j<s2.length())
-        {
-            if(m.find(s2[j])!=m.end())
-            {
-                m[s2[j]]--;
-                if(m[s2[j]]==0)
-                {
-                    cnt--;
-                }
+       int n = s1.length(), m = s2.length();
+        for(int i=0; i<=m-n; i++){
+            string temp = s2.substr(i, n);
+            if(isPermutation(s1,temp)){
+                return true;
             }
-            if(j-i+1 <k) j++;
-            else if(j-i+1 ==k)
-            {
-                if(cnt==0) return true;
-                if( m.find(s2[i])!=m.end())
-               {
-                m[s2[i]]++;
-                if(m[s2[i]]==1) cnt++;
-               }
-                 i++;
-                 j++;
-            }
-           
-             
         }
         return false;
     }
