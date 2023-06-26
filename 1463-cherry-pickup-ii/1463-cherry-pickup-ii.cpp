3310092b66e3c8d88ada7bea076dbb1c959ec76c
Yuvanna Bawa <76108856+YB73@users.noreply.github.com>
2023-06-27 00:18:27 +0530

    Time: 65 ms (90.35%), Space: 10 MB (82.71%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
+    int dy[3] = {0, -1, 1};
     int dp[71][71][71];
-    vector<pair<int,int>>v={{-1,-1},{-1,0},{-1,1},{0,-1},{0,0},{0,1},{1,-1},{1,0},{1,1}};
-    int helper(int i1,int j1,int j2,vector<vector<int>>&grid){
-        if(dp[i1][j1][j2]!=-1)return dp[i1][j1][j2];
-        int temp=0;
-        if((j1==j2)){
-            temp+=grid[i1][j1];
-        }
-        else temp+=grid[i1][j1]+grid[i1][j2];
-        int ans=temp;
-        for(int i=0;i<v.size();i++){
-            if((j1+v[i].first>=0)&&(j1+v[i].first<grid[0].size())&&(j2+v[i].second>=0)&&(j2+v[i].second<grid[0].size())&&(i1+1<grid.size())){
-                ans=max(ans,temp+helper(i1+1,j1+v[i].first,j2+v[i].second,grid));
+    
+    int ff(vector<vector<int>>& grid, int i, int c1, int c2) {
+        int m = grid.size();
+        int n = grid[0].size();
+        if (i == m) return 0;
+        if (c1 < 0 || c2 < 0 || c1 >= n || c2 >= n) return INT_MIN;
+        if (dp[i][c1][c2] != -1) return dp[i][c1][c2];
+        
+        int maxAns = 0;
+        
+        for (int k=0; k<3; k++) {
+            for (int r=0; r<3; r++) {
+                maxAns = max(maxAns, ff(grid, i + 1, c1 + dy[k], c2 + dy[r]));
             }
         }
-        return dp[i1][j1][j2]=ans;
+        
+        maxAns += (c1 == c2) ? grid[i][c1] : grid[i][c1] + grid[i][c2];
+        return dp[i][c1][c2] = maxAns;
     }
+    
     int cherryPickup(vector<vector<int>>& grid) {
-    memset(dp,-1,sizeof(dp));
-    return helper(0,0,grid[0].size()-1,grid);    
+        int m = grid.size();
+        if (!m) return 0;
+        int n = grid[0].size();
+        memset(dp, -1, sizeof dp);
+        return ff(grid, 0, 0, n-1);
     }
 };
