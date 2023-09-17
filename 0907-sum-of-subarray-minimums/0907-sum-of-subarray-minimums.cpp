90ac448abdbb7ad8ff448500d64093b48cd85456
Yuvanna Bawa <76108856+YB73@users.noreply.github.com>
2023-09-18 01:09:15 +0530

    Time: 69 ms (83.50%), Space: 41.2 MB (80.65%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int mod = 1e9+7;
+        int n=arr.size();
+        
+        vector<int> sum(n);
+        stack<int> st;
+        
+        int j,res=0;
+        
+        for(int i=0;i<n;i++){
+            
+            while(!st.empty() && arr[st.top()]>arr[i]) st.pop();
+            
+            j = !st.empty() ? st.top() : -1;
+            sum[i]= ((j>=0 ? sum[j] : 0)+ (i-j)*arr[i])%mod;
+            st.push(i);
+        }
+        
+        for(auto it : sum) res=(res+it)%mod;
+        
+        return res;
+    }
+};
