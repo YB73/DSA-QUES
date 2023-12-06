b3f94ebaa10ce8e7417e575ed48ff27ab6a283b0
Yuvanna Bawa <76108856+YB73@users.noreply.github.com>
2023-12-06 23:25:58 +0530

    Time: 3 ms (13.68%), Space: 6.4 MB (35.53%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+               int completeWeek = n / 7;
+        
+        //leftDays indicates number of left days after completeWeek turn.
+        int leftDays = n % 7;
+        
+        //starting money in the last week
+        int startMoney = completeWeek + 1;
+        
+        //sum of AP series : Sn = (n * [2 * a + (n - 1) * d]) / 2
+        int completeWeekMoneysum = (completeWeek * (2 * 28 + (completeWeek - 1) * 7)) / 2;
+        
+        int leftDaysMoneySum = (leftDays * (2 * startMoney + (leftDays - 1) * 1)) / 2;
+        
+        return (completeWeekMoneysum + leftDaysMoneySum);
+    }
+};
